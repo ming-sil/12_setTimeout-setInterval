81f59793a560c6193d0476b65e12d7218d0cfabd
ming-sil <choming4926@gmail.com>
2022-06-08 09:43:36 +0900

    Initial commit

---
+++ js/ex.js
@@ -0,0 +1,16 @@
+(function(){
+
+    const btn = document.querySelector('button');
+    const msgBox = document.querySelector('.message_box');
+
+    const handleClick = () => {
+        msgBox.classList.add('on');
+
+        setTimeout(() => {
+            msgBox.classList.remove('on')
+        },1000)
+    }
+
+    btn.addEventListener('click', handleClick);
+
+})();
---
+++ js/timefunction.js
@@ -0,0 +1,23 @@
+(function(){
+
+    // *setTimeout
+    // =>지정한 시간 내에 한번만 실행함
+    // =>setTimeout(() => {
+    //     실행식
+    // },시간)
+
+    // *setInterval
+    // =>지정한 시간 마다 계속 실행함
+    // =>setInterval(() => {
+    //     실행식
+    // },시간)
+
+
+    // setTimeout(() => {
+    //     document.write('2초 뒤에 한번만 실행함')
+    // }, 2000)
+
+    // const handleTime = () => {document.write('2초 뒤에 한번만 실행함')}
+    // setTimeout(handleTime, 2000)
+
+})();//end
